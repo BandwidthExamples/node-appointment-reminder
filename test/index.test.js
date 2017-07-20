e856a420728adc2050476f8aed5df0967bd02cf5
avbel <belchikovs@gmail.com>
2017-07-20 10:54:04 +0300

    Added tests for index

---
+++ test/index.test.js
@@ -0,0 +1,40 @@
+const test = require('ava');
+const td = require('testdouble');
+const supertest = require('supertest');
+
+const database = td.replace('../lib/database');
+const host = td.replace('../lib/host');
+
+const router = {routes: td.function(), allowedMethods: td.function()};
+td.replace('../lib/routes', router);
+const models = {User: {getByPhoneNumber: td.function()}};
+td.replace('../lib/models', models);
+const main = require('../lib/index');
+
+td.when(database()).thenResolve();
+td.when(router.allowedMethods()).thenReturn((ctx, next) => next());
+let user = null;
+td.when(router.routes()).thenReturn((ctx, next) => {
+	user = ctx.user;
+	return next();
+});
+td.when(host(td.matchers.anything(), td.matchers.anything())).thenDo((ctx, next) => {
+	ctx.session.userPhoneNumber = '+1234567890';
+	return next();
+});
+td.when(models.User.getByPhoneNumber('+1234567890')).thenResolve({});
+
+test.serial('GET / should render index page', async t => {
+	const app = await main();
+	await supertest(app.callback()).get('/')
+		.expect(200)
+		.expect('Content-Type', /html/);
+	t.pass();
+});
+
+test.serial('it should provide user data', async t => {
+	const app = await main();
+	user = null;
+	await supertest(app.callback()).get('/');
+	t.truthy(user);
+});
